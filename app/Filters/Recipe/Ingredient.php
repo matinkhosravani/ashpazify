9c88330908ccf73fa9cdd4e03cac40371c90b0b1
Matin Khosravani <matinkhosravani@gmail.com>
2024-07-14 01:11:42 +0330

    change included and excluded query params structure

--- app/Filters/Recipe/Ingredient.php
+++ app/Filters/Recipe/Ingredient.php
@@ -8,8 +8,8 @@ class Ingredient
 {
     public function handle($recipes, Closure $next)
     {
-        $included_ingredients = request('ingredients');
-        $excluded_ingredients = request('excluded_ingredients');
+        $included_ingredients = request()->has('ingredients') ? array_map('intval',explode(",", request('ingredients'))) : [];
+        $excluded_ingredients = request()->has('excluded_ingredients') ? array_map('intval',explode(",", request('excluded_ingredients'))) : [];
 
         if (!empty($included_ingredients)) {
             $recipes = $recipes->whereHas('ingredients', function ($q) use ($included_ingredients) {
